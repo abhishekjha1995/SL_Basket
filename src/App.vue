a65c54d078173a99540e887ba421b0f6ca8fd7a7
Abhishek Jha <abhishek.jha@newgen.co.in>
2019-05-08 16:48:36 +0530

    inital commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-app>
+    <v-content>
+      <Toolbar/>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+import Toolbar from './components/Toolbar';
+
+export default {
+  name: 'App',
+  components: {
+    Toolbar,
+  },
+  data() {
+    return {
+      //
+    };
+  },
+};
+</script>
---
+++ src/components/Toolbar.vue
@@ -0,0 +1,15 @@
+<template>
+	<v-toolbar dark color="primary">
+		<v-toolbar-side-icon></v-toolbar-side-icon>
+		<v-toolbar-title>Title</v-toolbar-title>
+		<v-spacer></v-spacer>
+		<v-toolbar-items class="hidden-sm-and-down">
+			<v-btn icon>
+				<v-icon>search</v-icon>
+			</v-btn>
+			<v-btn icon>
+				<v-icon>shopping_cart</v-icon>
+			</v-btn>
+		</v-toolbar-items>
+	</v-toolbar>
+</template>
